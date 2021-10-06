800df34bc117c345c62422ea6f3a3190c2c9b364
Sakib Hasan <sakibccr@gmail.com>
2021-10-06 16:21:56 +0600

    making cart persistence configurable

--- app/cart/utils.py
+++ app/cart/utils.py
@@ -1,7 +1,8 @@
 from shared_models.models import StoreCourseSection, StoreCertificate, CourseEnrollment, CertificateEnrollment
 
 
-def get_store_from_product(product):
+def get_store_from_product(products):
+    product = products.first()
     try:
         return product.store_course_section.store_course.store
     except StoreCourseSection.DoesNotExist:
@@ -45,68 +46,73 @@ def is_already_enrolled(product, profile):
     return False
 
 
-def get_formatted_data_and_price(product):
-    product_data = {}
+def get_formatted_data_and_price(products):
+    all_items = []
 
-    try:
-        store_certificate = StoreCertificate.objects.get(product=product)
-    except StoreCertificate.DoesNotExist:
-        pass
-    else:
-        product_data = {
-            'id': str(store_certificate.certificate.id),
-            'title': store_certificate.certificate.title,
-            'slug': store_certificate.certificate.slug,
-            'image_uri': store_certificate.certificate.certificate_image_uri.url if store_certificate.certificate.certificate_image_uri else store_certificate.certificate.external_image_url,
-            'external_image_url': store_certificate.certificate.external_image_url,
-            'provider': {
-                'id': store_certificate.certificate.course_provider.content_db_reference,
-                'code': store_certificate.certificate.course_provider.code
-            },
-            'price': product.fee,
-            'product_type': 'certificate'
-        }
+    for product in products:
+        product_data = {}
 
-    try:
-        store_course_section = StoreCourseSection.objects.get(product=product)
-    except StoreCourseSection.DoesNotExist:
-        pass
-    else:
-        product_data = {
-            'id': store_course_section.store_course.course.id,
-            'title': store_course_section.store_course.course.title,
-            'slug': store_course_section.store_course.course.slug,
-            'image_uri': store_course_section.store_course.course.course_image_uri.url if store_course_section.store_course.course.course_image_uri else store_course_section.store_course.course.external_image_url,
-            'external_image_url': store_course_section.store_course.course.external_image_url,
-            'provider': {
-                'id': store_course_section.store_course.course.course_provider.content_db_reference,
-                'code': store_course_section.store_course.course.course_provider.code
-            },
-            'product_type': 'store_course_section',
-            'section': {
-                'start_date': store_course_section.section.start_date,
-                'end_date': store_course_section.section.end_date,
-                'execution_site': store_course_section.section.execution_site,
-                'execution_mode': store_course_section.section.execution_mode
-            },
-            'price': product.fee,
-            'questionnaire': [
-                {
-                    'type': 'checkbox',
-                    'field': 'have_relevant_certificate',
-                    'label': 'Do you have a relevant certificate?'
+        try:
+            store_certificate = StoreCertificate.objects.get(product=product)
+        except StoreCertificate.DoesNotExist:
+            pass
+        else:
+            product_data = {
+                'id': str(store_certificate.certificate.id),
+                'title': store_certificate.certificate.title,
+                'slug': store_certificate.certificate.slug,
+                'image_uri': store_certificate.certificate.certificate_image_uri.url if store_certificate.certificate.certificate_image_uri else store_certificate.certificate.external_image_url,
+                'external_image_url': store_certificate.certificate.external_image_url,
+                'provider': {
+                    'id': store_certificate.certificate.course_provider.content_db_reference,
+                    'code': store_certificate.certificate.course_provider.code
                 },
-                {
-                    'type': 'text',
-                    'field': 'certificate_number',
-                    'label': 'Enter the certificate number'
-                },
-                {
-                    'type': 'date',
-                    'field': 'certificate_expiry_date',
-                    'label': 'Certificate expiry date'
-                }
-            ]
-        }
+                'price': product.fee,
+                'product_type': 'certificate'
+            }
 
-    return (product.fee, product_data)
+        try:
+            store_course_section = StoreCourseSection.objects.get(product=product)
+        except StoreCourseSection.DoesNotExist:
+            pass
+        else:
+            product_data = {
+                'id': store_course_section.store_course.course.id,
+                'title': store_course_section.store_course.course.title,
+                'slug': store_course_section.store_course.course.slug,
+                'image_uri': store_course_section.store_course.course.course_image_uri.url if store_course_section.store_course.course.course_image_uri else store_course_section.store_course.course.external_image_url,
+                'external_image_url': store_course_section.store_course.course.external_image_url,
+                'provider': {
+                    'id': store_course_section.store_course.course.course_provider.content_db_reference,
+                    'code': store_course_section.store_course.course.course_provider.code
+                },
+                'product_type': 'store_course_section',
+                'section': {
+                    'start_date': store_course_section.section.start_date,
+                    'end_date': store_course_section.section.end_date,
+                    'execution_site': store_course_section.section.execution_site,
+                    'execution_mode': store_course_section.section.execution_mode
+                },
+                'price': product.fee,
+                'questionnaire': [
+                    {
+                        'type': 'checkbox',
+                        'field': 'have_relevant_certificate',
+                        'label': 'Do you have a relevant certificate?'
+                    },
+                    {
+                        'type': 'text',
+                        'field': 'certificate_number',
+                        'label': 'Enter the certificate number'
+                    },
+                    {
+                        'type': 'date',
+                        'field': 'certificate_expiry_date',
+                        'label': 'Certificate expiry date'
+                    }
+                ]
+            }
+
+        all_items.append(product_data)
+
+    return all_items
--- app/cart/views/add.py
+++ app/cart/views/add.py
@@ -3,6 +3,7 @@ import base64
 from decimal import Decimal
 from django_scopes import scopes_disabled
 from django.utils import timezone
+from django.db.models import Sum
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -15,7 +16,7 @@ from rest_framework.status import HTTP_200_OK, HTTP_400_BAD_REQUEST
 
 from cart.auth import IsAuthenticated
 from cart.mixins import ResponseFormaterMixin
-from cart.tasks import generic_task_enqueue
+# from cart.tasks import generic_task_enqueue
 
 from cart.utils import get_store_from_product, is_already_enrolled, get_formatted_data_and_price
 from cart.serializers import StoreSerializer
@@ -27,161 +28,118 @@ class AddToCart(APIView, ResponseFormaterMixin):
     permission_classes = (IsAuthenticated, )
 
     def post(self, request, *args, **kwargs):
-        cart_id = request.data.get('cart_id', None)
-        product_id = request.data.get('product_id', None)
+        product_ids = request.data.get('product_ids', None)
         coupon_code = request.data.get('coupon_code', None)
         zip_code = request.data.get('zip_code', None)
+        persistent = request.data.get('persistent', None)
 
         # the permission class IsAuthenticated handles checkout=guest.
         # checkout = request.data.get('checkout', None)
 
-        #############################################################
-        # create cart block
-        #############################################################
+        # get the products first
 
-        if cart_id is None and product_id is None:
-            return Response({'message': 'cart id or product id must be provided'}, status=HTTP_200_OK)
+        products = Product.objects.filter(id__in=product_ids)
+        if not products.exists():
+            return Response({'message': 'product_ids must contain valid product ids'}, status=HTTP_200_OK)
 
-        # variable initialization
-        quantity = 1
-        discount_amount = Decimal('0.00')
-        sales_tax = Decimal('0.00')
-        tax_id = None
+        fee_aggregate = products.aggregate(total_amount=Sum('fee'))
+        total_amount = fee_aggregate['total_amount']
 
-        with scopes_disabled():
-            if cart_id is None:
-                try:
-                    product = Product.objects.get(id=product_id)
-                except Product.DoesNotExist:
-                    return Response({'message': 'Invalid product id'}, status=HTTP_400_BAD_REQUEST)
+        cart = None
+        if persistent:
+            cart = create_cart(products, total_amount, request.profile)  # cart must belong to a profile or guest
 
-                # before proceeding further, check the availability of current product for the current profile.
-                # if the profile is None e.g. guest, then the product is always available
-                if is_already_enrolled(product, request.profile):
-                    return Response({'message': 'Already enrolled in this course or certificate'}, status=HTTP_400_BAD_REQUEST)
+        if coupon_code:
+            coupon, discount_amount = coupon_apply(products, coupon_code, total_amount, request.profile, cart)
 
-                store = get_store_from_product(product)
+        if zip_code is not None and cart.store.tax_enabled:
+            sales_tax, tax_id = tax_apply(zip_code, products, cart)
 
-                cart = Cart.objects.create(
-                    profile=request.profile,
-                    store=store,
-                    coupon=None,
-                    status=Cart.STATUS_OPEN,
-                    note=None,
-                    extended_amount=product.fee,
-                    discount_amount=discount_amount,
-                    sales_tax=sales_tax,
-                    total_amount=product.fee,
-                    tax_id=tax_id,
-                    registration_details={},
-                    agreement_details={}
-                )
-
-                cart_id = cart.id
-
-            try:
-                cart = Cart.objects.get(id=cart_id)
-            except Cart.DoesNotExist:
-                return Response({'message': 'Invalid cart id'}, status=HTTP_400_BAD_REQUEST)
-
-                unit_price, product_data = get_formatted_data_and_price(product)
-                total_amount = unit_price * quantity
-
-                # applying coupon
-                try:
-                    coupon = Coupon.objects.get(store=cart.store, code=coupon_code)
-                    discount_amount, coupon_message = coupon_apply(coupon, cart, request.profile)
-                except Coupon.DoesNotExist:
-                    coupon = None
-                    coupon_message = 'coupon does not exist'
-
-                # apply tax
-                if zip_code is not None and cart.store.tax_enabled:
-                    sales_tax, tax_message, tax_id = tax_apply(zip_code, cart, product)
-
-                # calculating extended amount after getting tax and discount amounts
-                extended_amount = total_amount - discount_amount + sales_tax
-
-                # multiple items in a cart is not supported yet. so we take the first.
-                # when it will be supported, we will just create new cart item.
-
-                if cart.cart_items.exists():
-                    cart_item = cart.cart_items.first()
-                    cart_item.quantity = quantity
-                    cart_item.unit_price = unit_price
-                    cart_item.extended_amount = extended_amount
-                    cart_item.discount_amount = discount_amount
-                    cart_item.sales_tax = sales_tax
-                    cart_item.total_amount = total_amount
-                    cart_item.save()
-                else:
-                    cart_item = CartItem.objects.create(
-                        cart=cart,
-                        product=product,
-                        quantity=quantity,
-                        unit_price=unit_price,
-                        extended_amount=extended_amount,
-                        discount_amount=discount_amount,
-                        sales_tax=sales_tax,
-                        total_amount=total_amount,
-                    )
-
-                # now update the barren cart object with these values
-
-                # total_amount will be the sum of all cart_item's total amounts. however,
-                # right now only one cart item is supported. therefore, we can just use cart.total_amount = cart_item.total_amount amount.
-                cart.coupon = coupon
-                cart.total_amount = cart_item.total_amount
-                cart.extended_amount = extended_amount
-                cart.discount_amount = discount_amount
-                cart.sales_tax = sales_tax
-                cart.tax_id = tax_id
-                cart.save()
-
-                # other fields e.g. discount_amount and salex_tax will be set in appropriate places. here it's just 0.00
-
-                store_serializer = StoreSerializer(store)
-
-                payment_gateways = []
-                for item in StorePaymentGateway.objects.filter(store__url_slug=store.url_slug):
-                    payment_gateways.append({
-                        'id': str(item.id),
-                        'name': item.payment_gateway.name,
-                        'branding': item.branding
-                    })
-
-                data = {
-                    'product': product_data,
-                    'payment_gateways': payment_gateways,
-                    'cart_id': str(cart.id),
-                    'store': store_serializer.data,
-                    'questionnaire': [
-                        {
-                            'type': 'checkbox',
-                            'field': 'have_relevant_certificate',
-                            'label': 'Do you have a relevant certificate?'
-                        },
-                        {
-                            'type': 'text',
-                            'field': 'certificate_number',
-                            'label': 'Enter the certificate number'
-                        },
-                        {
-                            'type': 'date',
-                            'field': 'certificate_expiry_date',
-                            'label': 'Certificate expiry date'
-                        }
-                    ]
-                }
-
-                if cart.profile is not None:  # can not update crm if there is no zipcode
-                    generic_task_enqueue('crm_product.enroll', cart_id=str(cart.id), store_id=str(cart.store.id))
+        data = format_response(products, cart)
 
         return Response(self.object_decorator(data), status=HTTP_200_OK)
 
 
-def coupon_apply(coupon, cart, profile):
+def format_response(products, cart):
+    products = get_formatted_data_and_price(products)
+    store = get_store_from_product(products)
+    store_serializer = StoreSerializer(store)
+
+    payment_gateways = []
+    for item in StorePaymentGateway.objects.filter(store__url_slug=store.url_slug):
+        payment_gateways.append({
+            'id': str(item.id),
+            'name': item.payment_gateway.name,
+            'branding': item.branding
+        })
+
+    data = {
+        'product': products,
+        'payment_gateways': payment_gateways,
+        'cart_id': str(cart.id) if cart is not None else '',
+        'store': store_serializer.data,
+        'questionnaire': [
+            {
+                'type': 'checkbox',
+                'field': 'have_relevant_certificate',
+                'label': 'Do you have a relevant certificate?'
+            },
+            {
+                'type': 'text',
+                'field': 'certificate_number',
+                'label': 'Enter the certificate number'
+            },
+            {
+                'type': 'date',
+                'field': 'certificate_expiry_date',
+                'label': 'Certificate expiry date'
+            }
+        ]
+    }
+    return data
+
+
+def create_cart(products, total_amount, profile):
+    # the values set here will be updated twice:
+    # 1. once coupon is applied
+    # 2. once tax is applied
+    store = get_store_from_product(products)
+
+    with scopes_disabled():
+        cart = Cart.objects.create(
+            profile=profile,
+            store=store,
+            coupon=None,
+            status=Cart.STATUS_OPEN,
+            extended_amount=total_amount,
+            discount_amount=Decimal('0.00'),
+            sales_tax=Decimal('0.00'),
+            total_amount=total_amount,
+            note='',
+        )
+
+    # iterate products and create items
+    # quantity will be number of products
+    for product in products:
+        CartItem.objects.create(
+            quantity=1,
+            unit_price=product.fee,
+            extended_amount=product.fee,
+            discount_amount=Decimal('0.0'),
+            sales_tax=Decimal('0.0'),
+            total_amount=product.fee
+        )
+    return cart
+
+
+def coupon_apply(products, coupon_code, total_amount, profile, cart):
     # see if this coupon is fine or not
+
+    # applying coupon
+    try:
+        coupon = Coupon.objects.get(store=cart.store, code=coupon_code)
+    except Coupon.DoesNotExist:
+        return Response({'message': 'Coupon does not exist'}, status=HTTP_200_OK)
 
     today = timezone.now()
     if not coupon.is_active:
@@ -201,7 +159,7 @@ def coupon_apply(coupon, cart, profile):
     if coupon.amount < 0.00:
         coupon.amount = Decimal('0.00')
 
-    price_before_discount = cart.extended_amount
+    price_before_discount = total_amount
     discount = Decimal('0.0')
 
     if coupon.coupon_type == 'fixed':
@@ -212,38 +170,32 @@ def coupon_apply(coupon, cart, profile):
         # if coupon_type is not fixed, then it must be a percentage
         # coupon.max_limit makes sense here
 
-        discount = cart.extended_amount * coupon.amount / 100
+        discount = total_amount * coupon.amount / 100
 
         if coupon.max_limit is not None and discount > coupon.max_limit:
             discount = coupon.max_limit
 
-    price = cart.extended_amount - discount
+    price = total_amount - discount
 
     if price < 0.00:
         price = Decimal('0.00')
         discount = price_before_discount
 
-    cart.coupon = coupon
-    cart.discount = discount
-    cart.total_amount = price
-    cart.save()
+    if cart is not None:
+        cart.coupon = coupon
+        cart.discount = discount
+        cart.total_amount = price
+        cart.save()
 
-    # now, here we should also set these values for cart_item. let's try here. but doesn't feel dandy.
+        # now should we iterate over all cart item and apply the same discount to all the items???
 
-    cart_item = cart.cart_items.first()  # since cart doesn't support multiple items yet
-    cart_item.discount = discount
-    cart_item.total_amount = price
-    cart_item.save()
-
-    data = {
-        'discounted_price': price,
-        'discount': discount
-    }
-
-    return Response(data, status=HTTP_400_BAD_REQUEST)
+    return discount
 
 
-def tax_apply(zip_code, cart, product):
+def tax_apply(zip_code, products, cart):
+    # this whole thing has to be redone. because avatax allows calculating multiple products tax in just one request.
+    # so instead of iterating products, we should use that instead
+
     accountid = config('AVATAX_ACCOUNT_ID')
     license_key = config('AVATAX_LICENSE_KEY')
     company_code = config('AVATAX_COMPANY_CODE')
@@ -304,13 +256,8 @@ def tax_apply(zip_code, cart, product):
 
     if resp.status_code == 201:
         try:
-            tax_info = {'total_amount': resp_json['totalTax']}
+            return {'total_amount': resp_json['totalTax']}
         except KeyError:
-            return Response({'message': 'could not collect tax info'}, status=HTTP_400_BAD_REQUEST)
+            pass
 
-    try:
-        tax_amount = tax_info['total_amount']
-    except KeyError:
-        return Response({'message': 'Could not retrieve tax info. Is the provided address correct?'}, status=HTTP_400_BAD_REQUEST)
-
-    return Response(tax_info, status=HTTP_200_OK)
+    return Decimal('0.0')
